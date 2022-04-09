99a5801d3212741c31a8f5ef5844d1bf86e1b9f8
atmason90 <atmason90@gmail.com>
2022-04-08 22:14:26 -0700

    write pseudo code for js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Take city from form input and append to ul element
+// save city list items in local storage
+// get city list items from local storage so that list persists when page refreshes
+// Generate current weather conditions for searched city - openweather API
+    // city and date
+    // temperature
+    // humidity
+    // wind speed
+    // UV Index with color based on favorableness
+// Generate 5-Day Forecast for searched city - openweather API
+    // date
+    // weather condition icon
+    // temperature
+    // humidity
