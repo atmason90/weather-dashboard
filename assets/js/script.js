b1e6f44e086f85cfbb4b8911fb847becfed30d9f
atmason90 <atmason90@gmail.com>
2022-04-09 16:02:43 -0700

    re-format html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-// Take city from form input and append to ul element
+
+var cityFormEl = $("#cityInputForm");
+var cityListEl = $("#cityList");
+var input = $("#cityInput");
+var cityInputEl = "";
+// open weather API key
+var apiKey = "5d3f99ab5fd7f2680c22569bca5f3130"
+
+// Take city from form input and append to ul element. Save to local storage
+function handleFormSubmit(event) {
+    event.preventDefault();
+    cityInputEl = $("#cityInput").val().trim();
+    console.log(cityInputEl);
+    if(!cityInputEl) {
+        return;
+    }
+    cityListEl.append("<li>" + cityInputEl + "</li>");
+    $("input[name='cityNameInput']").val("");
+
+    localStorage.setItem("city", cityInputEl);
+}
+
+cityFormEl.on("submit", handleFormSubmit);
+
+
+
 // save city list items in local storage
 // get city list items from local storage so that list persists when page refreshes
 // Generate current weather conditions for searched city - openweather API
