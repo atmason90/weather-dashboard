e6173f480eb87df45bf5c49d97ef23af85425abb
atmason90 <atmason90@gmail.com>
2022-04-09 18:45:17 -0700

    get weather data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ var input = $("#cityInput");
 var cityInputEl = "";
 // open weather API key
 var apiKey = "5d3f99ab5fd7f2680c22569bca5f3130"
+var currentDate = moment().format("MMM Do, YYYY");
+$("#currentDate").text(currentDate);
+// $("#cityName").text(cityInputEl);
 
 // Take city from form input and append to ul element. Save to local storage
 function handleFormSubmit(event) {
@@ -15,6 +18,7 @@ function handleFormSubmit(event) {
         return;
     }
     cityListEl.append("<li>" + cityInputEl + "</li>");
+    $("#cityName").text(cityInputEl);
     $("input[name='cityNameInput']").val("");
 
     localStorage.setItem("city", cityInputEl);
@@ -22,7 +26,21 @@ function handleFormSubmit(event) {
 
 cityFormEl.on("submit", handleFormSubmit);
 
+// create a function to retrieve current date's weather data from openweather api
+function getWeather() {
+    // var currentDate = moment().format("MMM Do, YYYY");
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&units=imperial&appid=" + apiKey;
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
 
+        })
+};
+
+cityFormEl.on("submit", getWeather);
 
 // save city list items in local storage
 // get city list items from local storage so that list persists when page refreshes
